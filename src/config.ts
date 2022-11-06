4ed903b3751197cd55f7de292b4260d945dc809e
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-11-06 13:50:01 -0600

    okay ;

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const url: String = "http://localhost:8080/";
-// export const url: string =
-//   "https://cc4f2mf7kh.execute-api.us-east-2.amazonaws.com/dev/";
+// export const url: String = "http://localhost:8080/";
+export const url: string =
+  "https://cc4f2mf7kh.execute-api.us-east-2.amazonaws.com/dev/";
